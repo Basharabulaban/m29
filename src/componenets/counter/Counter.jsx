ae04c9fc221937c9e1cd77674e05b9bc86442f04
Basharabulaban <ba.ahmad@gmail.com>
2022-02-12 13:19:31 +0300

    commit counter reset button

--- src/componenets/counter/Counter.jsx
+++ src/componenets/counter/Counter.jsx
@@ -3,40 +3,80 @@ import PropTypes from "prop-types";
 import "./Counter.css";
 
 export default class Counter extends Component {
-    constructor() {
-        super();
-        this.state = {
-          counter: 0,  // total count
-        };
-    
-     this.increment = this.increment.bind(this);
-      }
-  
-    render() {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0, // total count
+    };
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+
+  render() {
     return (
       <div className="App">
-        <CounterButton by={2} incrementMethod={this.increment}  />
-        <CounterButton by={1} incrementMethod={this.increment}/>
-        <CounterButton by={5} incrementMethod={this.increment}/>
-        <CounterButton by={10} incrementMethod={this.increment}/>
-        <CounterButton by={15} incrementMethod={this.increment} />
-        <CounterButton by={20} incrementMethod={this.increment} />
+        <CounterButton
+          by={2}
+          incrementMethod={this.increment}
+          decrementMethod={this.decrement}
+        />
+
+        <CounterButton
+          by={1}
+          incrementMethod={this.increment}
+          decrementMethod={this.decrement}
+        />
+        <CounterButton
+          by={5}
+          incrementMethod={this.increment}
+          decrementMethod={this.decrement}
+        />
+        <CounterButton
+          by={10}
+          incrementMethod={this.increment}
+          decrementMethod={this.decrement}
+        />
+        <CounterButton
+          by={15}
+          incrementMethod={this.increment}
+          decrementMethod={this.decrement}
+        />
+        <CounterButton
+          by={20}
+          incrementMethod={this.increment}
+          decrementMethod={this.decrement}
+        />
+
         <span className="count">{this.state.counter}</span>
+
+        <div>
+          {" "}
+          <button className="reset" onClick={this.reset}>
+            Reset
+          </button>{" "}
+        </div>
       </div>
     );
   }
-
+  reset() {
+    this.setState( { counter: 0 });
+  }
   increment(by) {
     console.log(`increment from child ${by}`);
 
-    this.setState(
-    (prevState)=>{        
-      return {counter: prevState.counter + by}
-    }
-    
-    );
+    this.setState((prevState) => {
+      return { counter: prevState.counter + by };
+    });
   }
+  decrement(by) {
+    console.log(`decrement from child ${by}`);
 
+    this.setState((prevState) => {
+      return { counter: prevState.counter - by };
+    });
+  }
 }
 
 class CounterButton extends Component {
@@ -49,11 +89,15 @@ class CounterButton extends Component {
     };
 
     this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+    this.reset = this.reset.bind(this);
   }
   render() {
     return (
       <div className="Counter">
         <button onClick={this.increment}> + {this.props.by}</button>
+
+        <button onClick={this.decrement}> - {this.props.by}</button>
         <span className="count">{this.state.counter}</span>
       </div>
     );
@@ -61,13 +105,24 @@ class CounterButton extends Component {
   increment() {
     console.log("increment from parent");
 
-    this.setState(
-      (prevState)=> {
-      return {counter: prevState.counter + this.props.by}
-    }
-    );
+    this.setState((prevState) => {
+      return { counter: prevState.counter + this.props.by };
+    });
 
     this.props.incrementMethod(this.props.by);
+  }
+
+  decrement() {
+    console.log("decrement from parent");
+
+    this.setState((prevState) => {
+      return { counter: prevState.counter - this.props.by };
+    });
+
+    this.props.decrementMethod(this.props.by);
+  }
+  reset() {
+    this.setState( { counter: 0 });
   }
 }
 
