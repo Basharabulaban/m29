512bf3f4d9bdef49edb5a59d5eb00798d6f0c03b
Basharabulaban <ba.ahmad@gmail.com>
2022-02-19 02:05:03 +0300

    welcome Message

--- src/componenets/todo/WelcomeComponent.jsx
+++ src/componenets/todo/WelcomeComponent.jsx
@@ -1,18 +1,28 @@
 import react, { Component } from "react";
 export default class WelcomeComponent extends Component {
-    render() {
-      return (
-      
-       <div>
+  render() {
+    return (
+      <div>
         <h1>Welcome!</h1>
-<div className="container">
-
-
-Welcome You {this.props.params.name} . You can manage your Todos{" "}
+        <div className="container">
+          Welcome You {this.props.params.name} . You can manage your Todos{" "}
           <a href="/todos"> Here </a>{" "}
         </div>
-</div>
-
-      );
-    }
+        <div className="container">
+          click here to get customize welcome message {this.props.params.name} .
+          <button
+            onClick={this.retrieveWelcomeMessage}
+            className="btn btn-success"
+          >
+            
+            Get Welcome Message
+          </button>
+          You can manage your Todos <a href="/todos"> Here </a>{" "}
+        </div>
+      </div>
+    );
   }
+   retrieveWelcomeMessage() {
+    console.log("click message welcome button") 
+  } 
+}
