9e59b747b589534f496b70eb61bbf4248df27586
Basharabulaban <ba.ahmad@gmail.com>
2022-02-20 20:34:46 +0300

    connect react with list restfull services

---
+++ src/api/todo/TodoDataService.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+class TodoDataService {
+    executeHelloWorldService() {
+        console.log("executed executeHelloWorldService ");
+        return axios.get("http://localhost:8083/hi");
+        
+      }
+    
+      executeHelloWorldbeanService() {
+        console.log(" executeHelloWorldbeanService ");
+        return axios.get("http://localhost:8083/hello-world-been");
+        
+      }
+      RetrieveAllTodos(name) {
+        console.log("executeRetrieveTodos ");
+        return axios.get(`http://localhost:8083/users/${name}/todos`);
+        
+      }
+
+}
+
+export default new TodoDataService();
--- src/componenets/todo/ListTodoCompnent.jsx
+++ src/componenets/todo/ListTodoCompnent.jsx
@@ -1,39 +1,62 @@
 import react, { Component } from "react";
+import TodoDataService from "../../api/todo/TodoDataService";
+import AuthenticationService from "./AuthenticationService";
+
 export default class ListTodoCompnent extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        todos: [
-          {
-            id: 1,
-            description: "learn react",
-            done: false,
-            targetDate: new Date(),
-          },
-          {
-            id: 2,
-            description: "learn Java",
-            done: false,
-            targetDate: new Date(),
-          },
-          {
-            id: 3,
-            description: "learn Microservise",
-            done: false,
-            targetDate: new Date(),
-          },
-          { id: 4, description: "learn OS", done: false, targetDate: new Date() },
-        ],
-      };
-    }
-  
-    render() {
-      return (
-    
-       <div>
-       <h1> List To Do </h1>
-       <div className="container"> 
-          
+  constructor(props) {
+    // initialize commponenet
+    super(props);
+    this.state = {
+      todos: [
+        // {
+        //   id: 1,
+        //   description: "learn react",
+        //   done: false,
+        //   targetDate: new Date(),
+        // },
+        // {
+        //   id: 2,
+        //   description: "learn Java",
+        //   done: false,
+        //   targetDate: new Date(),
+        // },
+        // {
+        //   id: 3,
+        //   description: "learn Microservise",
+        //   done: false,
+        //   targetDate: new Date(),
+        // },
+        // { id: 4, description: "learn OS", done: false, targetDate: new Date() },
+      ],
+    };
+  }
+  componentDidMount() {
+    // once the data is apear
+    TodoDataService.RetrieveAllTodos(
+      AuthenticationService.getLoggedInUsersName()
+    ).then((response) => {
+      this.handleSucessResponse(response);
+      // this.setState({ todos: response.data });
+      console.log(response);
+    });
+    // .catch (error => this.handleError(error))
+  }
+  handleError(error) {
+    console.log(error.response);
+    this.setState({ todos: error.response.data.message });
+  }
+
+  handleSucessResponse(response) {
+    this.setState({ todos: response.data });
+  }
+
+  //loaded for the first time and show on the brouser
+  render() {
+    // whenver state has changes // render
+    return (
+      <div>
+        <h1> List To Do </h1>
+        <div className="container">
           <table>
             <thead>
               <tr>
@@ -60,20 +83,19 @@ export default class ListTodoCompnent extends Component {
   todos.map (todo=>todo.id)
               
               */}
-  
-              {this.state.todos.map(todo => 
+
+              {this.state.todos.map((todo) => (
                 <tr>
                   <td>{todo.id}</td>
                   <td>{todo.done.toString()}</td>
                   <td>{todo.description}</td>
                   <td>{todo.targetDate.toString()}</td>
                 </tr>
-              )}
-
+              ))}
             </tbody>
           </table>
         </div>
-        </div>
-      );
-    }
+      </div>
+    );
   }
+}
