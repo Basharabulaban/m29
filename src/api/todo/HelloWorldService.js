db7b9219875cd455d3d7b11b14d022888ba4fc66
Basharabulaban <ba.ahmad@gmail.com>
2022-03-05 17:41:12 +0300

    fix welcome message

--- src/api/todo/HelloWorldService.js
+++ src/api/todo/HelloWorldService.js
@@ -1,23 +1,51 @@
 import axios from "axios";
 
 class HelloWorldService {
-
   executeHelloWorldService() {
+    let username = "basharusr";
+    let password = "basharpw";
+    let basicAuthentication = "Basic " + window.btoa(`${username}:${password}`);
     console.log("executed executeHelloWorldService ");
-    return axios.get("http://localhost:8083/hi");
-    
+    return axios.get(
+      "http://localhost:8083/hi",
+
+      {
+        headers: {
+          Authorization: basicAuthentication,
+        },
+      }
+    );
   }
 
   executeHelloWorldbeanService() {
     console.log(" executeHelloWorldbeanService ");
-    return axios.get("http://localhost:8083/hello-world-been");
-    
+    let username = "basharusr";
+    let password = "basharpw";
+    let basicAuthentication = "Basic " + window.btoa(`${username}:${password}`);
+
+    return axios.get(
+      "http://localhost:8083/hello-world-been",
+
+      {
+        headers: {
+          Authorization: basicAuthentication,
+        },
+      }
+    );
   }
   executehelloworldpathvariable(name) {
+    let username = "basharusr";
+    let password = "basharpw";
+    let basicAuthentication = "Basic " + window.btoa(`${username}:${password}`);
     console.log(" executehelloworldpathvariable ");
-    return axios.get(`http://localhost:8083/hello-world-path-variable/${name}`);
-    
+    return axios.get(
+      `http://localhost:8083/hello-world-path-variable/${name}`,
+      {
+        headers: {
+          Authorization: basicAuthentication,
+        },
+      }
+    );
   }
-  
 }
 export default new HelloWorldService();
