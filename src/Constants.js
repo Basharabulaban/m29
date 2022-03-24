f0307e761d5f4ac08e56c16c373f095c8202b092
Basharabulaban <ba.ahmad@gmail.com>
2022-03-24 09:40:56 +0300

    what about openshift

--- src/Constants.js
+++ src/Constants.js
@@ -1,4 +1,5 @@
 
 export const USER_NAME_SESSION_ATTRIBUTE_NAME="authenticatedUser"
 export const API_URL = "http://localhost:8083"
+// openshift ??
 export const API_JPA_URL = "http://localhost:8083/jpa"
